0becc6320b33e1ed311d3252eeb63adb0e8074c5
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-27 18:52:47 -0300

    test: ensure AddNaturalPersonController returns serverError if Validation throws

--- tests/presentation/controllers/add-natural-person-controller.spec.ts
+++ tests/presentation/controllers/add-natural-person-controller.spec.ts
@@ -15,4 +15,19 @@ describe('AddNaturalPersonController', () => {
     await sut.handle({})
     expect(validationSpy.input).toEqual({})
   })
+
+  test('Should return serverError if Validation throws', async() => {
+    class ValidationSpy implements Validation {
+      public input: object
+
+      public validate(input: object): void {
+        this.input = input
+      }
+    }
+    const validationSpy = new ValidationSpy()
+    jest.spyOn(validationSpy, 'validate').mockImplementationOnce(() => { throw new Error() })
+    const sut = new AddNaturalPersonController(validationSpy)
+    const httpResponse = await sut.handle({})
+    expect(httpResponse).toEqual({ statusCode: 500, body: new Error() })
+  })
 })
