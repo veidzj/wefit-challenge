0fb9b1c9219feaca1cfaf049311d056893795fad
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-27 19:00:38 -0300

    refactor: move sut creation to a factory

--- tests/presentation/controllers/add-natural-person-controller.spec.ts
+++ tests/presentation/controllers/add-natural-person-controller.spec.ts
@@ -1,32 +1,30 @@
+import { ValidationSpy } from '@/tests/presentation/mocks'
 import { AddNaturalPersonController } from '@/presentation/controllers'
-import { type Validation } from '../protocols'
+
+interface Sut {
+  sut: AddNaturalPersonController
+  validationSpy: ValidationSpy
+}
+
+const makeSut = (): Sut => {
+  const validationSpy = new ValidationSpy()
+  const sut = new AddNaturalPersonController(validationSpy)
+  return {
+    sut,
+    validationSpy
+  }
+}
 
 describe('AddNaturalPersonController', () => {
   test('Should call Validation with correct values', async() => {
-    class ValidationSpy implements Validation {
-      public input: object
-
-      public validate(input: object): void {
-        this.input = input
-      }
-    }
-    const validationSpy = new ValidationSpy()
-    const sut = new AddNaturalPersonController(validationSpy)
+    const { sut, validationSpy } = makeSut()
     await sut.handle({})
     expect(validationSpy.input).toEqual({})
   })
 
   test('Should return serverError if Validation throws', async() => {
-    class ValidationSpy implements Validation {
-      public input: object
-
-      public validate(input: object): void {
-        this.input = input
-      }
-    }
-    const validationSpy = new ValidationSpy()
+    const { sut, validationSpy } = makeSut()
     jest.spyOn(validationSpy, 'validate').mockImplementationOnce(() => { throw new Error() })
-    const sut = new AddNaturalPersonController(validationSpy)
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual({ statusCode: 500, body: new Error() })
   })
