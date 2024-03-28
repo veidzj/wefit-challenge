590bfd796f597382fce4c7c360b2b1aade344083
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-28 17:11:55 -0300

    feat: ensure AddNaturalPersonController calls Validation with correct values

---
+++ src/domain/models/address.ts
@@ -0,0 +1,9 @@
+export interface Address {
+  cep: string
+  street: string
+  number: number
+  complement: string
+  city: string
+  neighborhood: string
+  state: string
+}
--- src/presentation/controllers/add-natural-person-controller.ts
+++ src/presentation/controllers/add-natural-person-controller.ts
@@ -1,11 +1,12 @@
 import { type Controller, type HttpResponse, type Validation } from '@/presentation/protocols'
 import { HttpHelper } from '@/presentation/helpers'
 import { ValidationError } from '@/validation/errors'
+import { type Address } from '@/domain/models'
 
 export class AddNaturalPersonController implements Controller {
   constructor(private readonly validation: Validation) {}
 
-  public async handle(request: object): Promise<HttpResponse> {
+  public async handle(request: AddNaturalPersonController.Request): Promise<HttpResponse> {
     try {
       this.validation.validate(request)
       return {
@@ -18,5 +19,17 @@ export class AddNaturalPersonController implements Controller {
       }
       return HttpHelper.serverError(error as Error)
     }
+  }
+}
+
+export namespace AddNaturalPersonController {
+  export interface Request {
+    cpf: string
+    name: string
+    cellPhone: string
+    phone: string
+    email: string
+    emailConfirmation: string
+    address: Address
   }
 }
