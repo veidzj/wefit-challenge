7963788b213f5815c9ba36793f1c97226610edd3
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-28 04:57:07 -0300

    refactor: move sut creation to a factory

--- tests/validation/validators/cnpj-validation.spec.ts
+++ tests/validation/validators/cnpj-validation.spec.ts
@@ -3,12 +3,25 @@ import { faker } from '@faker-js/faker'
 import { CNPJValidatorSpy } from '@/tests/validation/mocks'
 import { CNPJValidation } from '@/validation/validators'
 
+interface Sut {
+  sut: CNPJValidation
+  cnpjValidatorSpy: CNPJValidatorSpy
+}
+
+const makeSut = (): Sut => {
+  const cnpjValidatorSpy = new CNPJValidatorSpy()
+  const sut = new CNPJValidation(fieldName, cnpjValidatorSpy)
+  return {
+    sut,
+    cnpjValidatorSpy
+  }
+}
+
 const fieldName: string = faker.word.words()
 
 describe('CNPJValidation', () => {
   test('Should call CNPJValidator with correct cnpj', () => {
-    const cnpjValidatorSpy = new CNPJValidatorSpy()
-    const sut = new CNPJValidation(fieldName, cnpjValidatorSpy)
+    const { sut, cnpjValidatorSpy } = makeSut()
     const cnpj = faker.string.numeric(14)
 
     sut.validate({
