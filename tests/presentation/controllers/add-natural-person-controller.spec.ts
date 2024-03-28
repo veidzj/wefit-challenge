e8fbafd9ad3bab854e6cf46b7ee0a9741bae85f2
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-28 17:41:47 -0300

    test: ensure AddNaturalPersonController calls Validation with correct values

--- tests/presentation/controllers/add-natural-person-controller.spec.ts
+++ tests/presentation/controllers/add-natural-person-controller.spec.ts
@@ -19,25 +19,50 @@ const makeSut = (): Sut => {
   }
 }
 
+const mockRequest = (): AddNaturalPersonController.Request => ({
+  cpf: faker.string.numeric(11),
+  name: faker.person.fullName(),
+  cellPhone: faker.phone.number(),
+  phone: faker.phone.number(),
+  email: faker.internet.email(),
+  emailConfirmation: faker.internet.email(),
+  address: {
+    cep: faker.location.zipCode(),
+    street: faker.location.street(),
+    number: faker.number.int(1000),
+    complement: faker.location.secondaryAddress(),
+    city: faker.location.city(),
+    neighborhood: faker.location.secondaryAddress(),
+    state: faker.location.state()
+  }
+})
+
 describe('AddNaturalPersonController', () => {
   test('Should call Validation with correct values', async() => {
     const { sut, validationSpy } = makeSut()
-    await sut.handle({})
-    expect(validationSpy.input).toEqual({})
+    const request = mockRequest()
+
+    await sut.handle(request)
+
+    expect(validationSpy.input).toEqual(request)
   })
 
   test('Should return badRequest if Validation throws ValidationError', async() => {
     const { sut, validationSpy } = makeSut()
     const errorMessage = faker.word.words()
     jest.spyOn(validationSpy, 'validate').mockImplementationOnce(() => { throw new ValidationError(errorMessage) })
-    const httpResponse = await sut.handle({})
+
+    const httpResponse = await sut.handle(mockRequest())
+
     expect(httpResponse).toEqual(HttpHelper.badRequest(new ValidationError(errorMessage)))
   })
 
   test('Should return serverError if Validation throws', async() => {
     const { sut, validationSpy } = makeSut()
     jest.spyOn(validationSpy, 'validate').mockImplementationOnce(() => { throw new Error() })
-    const httpResponse = await sut.handle({})
+
+    const httpResponse = await sut.handle(mockRequest())
+
     expect(httpResponse).toEqual(HttpHelper.serverError(new Error()))
   })
 })
