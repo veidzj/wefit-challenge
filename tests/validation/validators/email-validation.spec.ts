d20fec62aad9e3f71e3d46b561ee266187a606aa
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-28 02:02:10 -0300

    refactor: move sut creation to a factory

--- tests/validation/validators/email-validation.spec.ts
+++ tests/validation/validators/email-validation.spec.ts
@@ -1,23 +1,31 @@
 import { faker } from '@faker-js/faker'
 
 import { EmailValidation } from '@/validation/validators'
-import { type EmailValidator } from '@/validation/protocols'
+import { EmailValidatorSpy } from '@/tests/validation/mocks'
+
+interface Sut {
+  sut: EmailValidation
+  emailValidatorSpy: EmailValidatorSpy
+}
+
+const makeSut = (): Sut => {
+  const emailValidatorSpy = new EmailValidatorSpy()
+  const sut = new EmailValidation(fieldName, emailValidatorSpy)
+  return {
+    sut,
+    emailValidatorSpy
+  }
+}
+
+const fieldName: string = faker.word.words()
 
 describe('EmailValidation', () => {
   test('Should call EmailValidator with correct email', () => {
-    class EmailValidatorSpy implements EmailValidator {
-      public email: string
-
-      public isValid(email: string): boolean {
-        this.email = email
-        return true
-      }
-    }
-    const emailValidatorSpy = new EmailValidatorSpy()
-    const fieldName: string = faker.word.words()
-    const sut = new EmailValidation(fieldName, emailValidatorSpy)
+    const { sut, emailValidatorSpy } = makeSut()
     const email = faker.internet.email()
+
     sut.validate({ [fieldName]: email })
+
     expect(emailValidatorSpy.email).toBe(email)
   })
 })
