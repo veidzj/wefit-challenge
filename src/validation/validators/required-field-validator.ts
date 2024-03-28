62712e040492c2322bd9034ad43ecea0e2e3cfc8
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-27 23:51:46 -0300

    feat: ensure RequiredFieldValidator throws ValidationError if field is not provided

---
+++ src/validation/validators/required-field-validator.ts
@@ -0,0 +1,12 @@
+import { ValidationError } from '@/validation/errors'
+import { type Validation } from '@/presentation/protocols'
+
+export class RequiredFieldValidator implements Validation {
+  constructor(private readonly fieldName: string) {}
+
+  public validate(input: object): void {
+    if (!input[(this.fieldName)]) {
+      throw new ValidationError(`${this.fieldName} is required`)
+    }
+  }
+}
