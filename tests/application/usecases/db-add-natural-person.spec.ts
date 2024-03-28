299985df5f43562c8497fc591da2e015b5fbc3ef
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-28 18:48:04 -0300

    refactor: move sut creation to a factory

--- tests/application/usecases/db-add-natural-person.spec.ts
+++ tests/application/usecases/db-add-natural-person.spec.ts
@@ -1,38 +1,24 @@
-import { faker } from '@faker-js/faker'
-
+import { AddNaturalPersonRepositorySpy, mockAddNaturalPersonRepositoryInput } from '@/tests/application/mocks'
 import { DbAddNaturalPerson } from '@/application/usecases'
-import { type AddNaturalPersonRepository } from '@/application/protocols'
 
-const mockAddNaturalPersonRepositoryInput = (): AddNaturalPersonRepository.Input => ({
-  cpf: faker.string.numeric(11),
-  name: faker.person.fullName(),
-  cellPhone: faker.phone.number(),
-  phone: faker.phone.number(),
-  email: faker.internet.email(),
-  emailConfirmation: faker.internet.email(),
-  address: {
-    cep: faker.location.zipCode(),
-    street: faker.location.street(),
-    number: faker.number.int(1000),
-    complement: faker.location.secondaryAddress(),
-    city: faker.location.city(),
-    neighborhood: faker.location.secondaryAddress(),
-    state: faker.location.state()
+interface Sut {
+  sut: DbAddNaturalPerson
+  addNaturalPersonRepositorySpy: AddNaturalPersonRepositorySpy
+}
+
+const makeSut = (): Sut => {
+  const addNaturalPersonRepositorySpy = new AddNaturalPersonRepositorySpy()
+  const sut = new DbAddNaturalPerson(addNaturalPersonRepositorySpy)
+  return {
+    sut,
+    addNaturalPersonRepositorySpy
   }
-})
+}
 
 describe('DbAddNaturalPerson', () => {
   describe('AddNaturalPersonRepository', () => {
     test('Should call AddNaturalPersonRepository with correct values', async() => {
-      class AddNaturalPersonRepositorySpy implements AddNaturalPersonRepository {
-        public input: AddNaturalPersonRepository.Input
-
-        public async add(input: AddNaturalPersonRepository.Input): Promise<void> {
-          this.input = input
-        }
-      }
-      const addNaturalPersonRepositorySpy = new AddNaturalPersonRepositorySpy()
-      const sut = new DbAddNaturalPerson(addNaturalPersonRepositorySpy)
+      const { sut, addNaturalPersonRepositorySpy } = makeSut()
       const addNaturalPersonRepositoryInput = mockAddNaturalPersonRepositoryInput()
 
       await sut.add(addNaturalPersonRepositoryInput)
