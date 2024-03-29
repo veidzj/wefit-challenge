14d49e5bfa442b94a37e95b1684466e138eae4c7
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-29 18:35:01 -0300

    test: ensure ValidationComposite throws ValidationError if any validation throws

---
+++ tests/validation/validators/validation-composite.spec.ts
@@ -0,0 +1,31 @@
+import { faker } from '@faker-js/faker'
+
+import { ValidationSpy } from '@/tests/presentation/mocks'
+import { ValidationComposite } from '@/validation/validators'
+import { ValidationError } from '@/validation/errors'
+
+interface Sut {
+  sut: ValidationComposite
+  validationSpies: ValidationSpy[]
+}
+
+const makeSut = (): Sut => {
+  const validationSpies = [new ValidationSpy(), new ValidationSpy()]
+  const sut = new ValidationComposite(validationSpies)
+  return {
+    sut,
+    validationSpies
+  }
+}
+
+describe('ValidationComposite', () => {
+  test('Should throw ValidationError if any validation throws', () => {
+    const { sut, validationSpies } = makeSut()
+    const errorMessage = faker.word.words()
+    jest.spyOn(validationSpies[0], 'validate').mockImplementationOnce(() => { throw new ValidationError(errorMessage) })
+
+    expect(() => {
+      sut.validate({ name: faker.word.words() })
+    }).toThrow(new ValidationError(errorMessage))
+  })
+})
