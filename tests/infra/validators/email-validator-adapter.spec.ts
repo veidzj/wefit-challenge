b4dab7391118909a61ebe9b31d055478c3ff6e91
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-29 15:50:21 -0300

    test: ensure EmailValidatorAdapter calls isEmail with correct email

---
+++ tests/infra/validators/email-validator-adapter.spec.ts
@@ -0,0 +1,26 @@
+import validator from 'validator'
+import { faker } from '@faker-js/faker'
+
+import { EmailValidatorAdapter } from '@/infra/validators'
+
+jest.mock('validator', () => ({
+  isEmail(): boolean {
+    return true
+  }
+}))
+
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter()
+}
+
+describe('EmailValidatorAdapter', () => {
+  test('Should call isEmail with correct email', () => {
+    const sut = makeSut()
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+    const email = faker.internet.email()
+
+    sut.isValid(email)
+
+    expect(isEmailSpy).toHaveBeenCalledWith(email)
+  })
+})
