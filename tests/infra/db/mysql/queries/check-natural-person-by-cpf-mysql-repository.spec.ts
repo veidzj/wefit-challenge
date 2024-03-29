ca1415607df6b48e77109abb7a6e162fb1f8c1ab
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-29 14:35:56 -0300

    test: ensure CheckNaturalPersonByCPFMySQLRepository returns true on success

---
+++ tests/infra/db/mysql/queries/check-natural-person-by-cpf-mysql-repository.spec.ts
@@ -0,0 +1,33 @@
+import { connectToDatabase, disconnectFromDatabase, clearTable } from '@/tests/infra/db/mysql'
+import { mockAddNaturalPersonRepositoryInput } from '@/tests/application/mocks'
+import { CheckNaturalPersonByCPFMySQLRepository } from '@/infra/db/mysql/queries'
+import { MySQLHelper } from '@/infra/db/mysql/helpers'
+
+describe('CheckNaturalPersonByCPFMySQLRepository', () => {
+  beforeAll(async() => {
+    await connectToDatabase()
+  })
+
+  afterAll(async() => {
+    await disconnectFromDatabase()
+  })
+
+  afterEach(async() => {
+    await clearTable('natural_person')
+  })
+
+  test('Should return true on success', async() => {
+    const mySQLHelper = MySQLHelper.getInstance()
+    const connection = mySQLHelper.getConnection()
+    const addNaturalPersonRepositoryInput = mockAddNaturalPersonRepositoryInput()
+    await connection.execute(
+      'INSERT INTO natural_person (cpf, name, cellPhone, phone, email, cep, street, number, complement, city, neighborhood, state) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+      [addNaturalPersonRepositoryInput.cpf, addNaturalPersonRepositoryInput.name, addNaturalPersonRepositoryInput.cellPhone, addNaturalPersonRepositoryInput.phone, addNaturalPersonRepositoryInput.email, addNaturalPersonRepositoryInput.address.cep, addNaturalPersonRepositoryInput.address.street, addNaturalPersonRepositoryInput.address.number, addNaturalPersonRepositoryInput.address.complement, addNaturalPersonRepositoryInput.address.city, addNaturalPersonRepositoryInput.address.neighborhood, addNaturalPersonRepositoryInput.address.state]
+    )
+    const sut = new CheckNaturalPersonByCPFMySQLRepository()
+
+    const result = await sut.check(addNaturalPersonRepositoryInput.cpf)
+
+    expect(result).toBe(true)
+  })
+})
