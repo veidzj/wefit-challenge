5e11e49d78a407ff5777b560de127727cfadfef7
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-29 23:45:53 -0300

    feat: ensure AddLegalPersonController returns badRequest if Validation throws ValidationError

--- src/presentation/controllers/add-legal-person-controller.ts
+++ src/presentation/controllers/add-legal-person-controller.ts
@@ -1,12 +1,20 @@
 import { type Controller, type HttpResponse, type Validation } from '@/presentation/protocols'
 import { HttpHelper } from '@/presentation/helpers'
+import { ValidationError } from '@/validation/errors'
 
 export class AddLegalPersonController implements Controller {
   constructor(private readonly validation: Validation) {}
 
   public async handle(request: AddLegalPersonController.Request): Promise<HttpResponse> {
-    this.validation.validate(request)
-    return HttpHelper.ok({})
+    try {
+      this.validation.validate(request)
+      return HttpHelper.ok({})
+    } catch (error) {
+      if (error instanceof ValidationError) {
+        return HttpHelper.badRequest(error)
+      }
+      return HttpHelper.serverError(error as Error)
+    }
   }
 }
 
