c128a3da8e9803310765ac12649901b7905bbfeb
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-30 01:14:51 -0300

    test: ensure CheckLegalPersonByCNPJMySQLRepository returns true if there is a legal person with provided cnpj

--- tests/infra/db/mysql/queries/check-legal-person-by-cpf-mysql-repository.spec.ts
+++ tests/infra/db/mysql/queries/check-legal-person-by-cpf-mysql-repository.spec.ts
@@ -1,6 +1,7 @@
 import { connectToDatabase, disconnectFromDatabase, clearTable } from '@/tests/infra/db/mysql'
 import { mockAddLegalPersonRepositoryInput } from '@/tests/application/mocks'
 import { CheckLegalPersonByCNPJMySQLRepository } from '@/infra/db/mysql/queries'
+import { MySQLHelper } from '@/infra/db/mysql/helpers'
 
 describe('CheckLegalPersonByCNPJMySQLRepository', () => {
   beforeAll(async() => {
@@ -21,5 +22,20 @@ describe('CheckLegalPersonByCNPJMySQLRepository', () => {
     const result = await sut.check(mockAddLegalPersonRepositoryInput().cnpj)
 
     expect(result).toBe(false)
+  })
+
+  test('Should return true if there is a legal person with provided cnpj', async() => {
+    const mySQLHelper = MySQLHelper.getInstance()
+    const connection = mySQLHelper.getConnection()
+    const addLegalPersonRepositoryInput = mockAddLegalPersonRepositoryInput()
+    await connection.execute(
+      'INSERT INTO legal_person (cnpj, responsibleCpf, name, cellPhone, phone, email, cep, street, number, complement, city, neighborhood, state) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+      [addLegalPersonRepositoryInput.cnpj, addLegalPersonRepositoryInput.responsibleCpf, addLegalPersonRepositoryInput.name, addLegalPersonRepositoryInput.cellPhone, addLegalPersonRepositoryInput.phone, addLegalPersonRepositoryInput.email, addLegalPersonRepositoryInput.cep, addLegalPersonRepositoryInput.street, addLegalPersonRepositoryInput.number, addLegalPersonRepositoryInput.complement, addLegalPersonRepositoryInput.city, addLegalPersonRepositoryInput.neighborhood, addLegalPersonRepositoryInput.state]
+    )
+    const sut = new CheckLegalPersonByCNPJMySQLRepository()
+
+    const result = await sut.check(addLegalPersonRepositoryInput.cnpj)
+
+    expect(result).toBe(true)
   })
 })
