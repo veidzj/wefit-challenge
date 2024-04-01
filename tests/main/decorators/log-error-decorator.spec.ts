d32a0fb84cd60a490f8b5d995214d76e2bee325f
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-04-01 05:08:16 -0300

    test: ensure LogErrorDecorator calls LogErrorRepositoy with correct error if controller returns a server error

--- tests/main/decorators/log-error-decorator.spec.ts
+++ tests/main/decorators/log-error-decorator.spec.ts
@@ -3,6 +3,8 @@ import { faker } from '@faker-js/faker'
 import { ControllerSpy } from '@/tests/presentation/mocks'
 import { LogErrorRepositorySpy } from '@/tests/application/mocks'
 import { LogErrorDecorator } from '@/main/decorators'
+import { type HttpResponse } from '@/presentation/protocols'
+import { HttpHelper } from '@/presentation/helpers'
 
 interface Sut {
   sut: LogErrorDecorator
@@ -25,22 +27,28 @@ const mockRequest = (): object => ({
   field: faker.word.words()
 })
 
+const mockServerError = (): HttpResponse => {
+  const fakeError = new Error()
+  fakeError.stack = faker.word.words()
+  return HttpHelper.serverError(fakeError)
+}
+
 describe('LogErrorDecorator', () => {
   describe('Controller', () => {
     test('Should call Controller with correct values', async() => {
       const { sut, controllerSpy } = makeSut()
       const request = mockRequest()
-  
+
       await sut.handle(request)
-  
+
       expect(controllerSpy.request).toEqual(request)
     })
-  
+
     test('Should return the same result of the controller', async() => {
       const { sut, controllerSpy } = makeSut()
-  
+
       const httpResponse = await sut.handle(mockRequest())
-  
+
       expect(httpResponse).toEqual(controllerSpy.httpResponse)
     })
   })
@@ -54,6 +62,18 @@ describe('LogErrorDecorator', () => {
 
       expect(logErrorRepositorySpy.log).toHaveBeenCalledTimes(0)
       expect(logErrorRepositorySpy.stack).toBeUndefined()
+    })
+
+    test('Should call LogErrorRepositoy with correct error if controller returns a server error', async() => {
+      const { sut, controllerSpy, logErrorRepositorySpy } = makeSut()
+      jest.spyOn(logErrorRepositorySpy, 'log')
+      const serverError = mockServerError()
+      controllerSpy.httpResponse = serverError
+
+      await sut.handle(mockRequest())
+
+      expect(logErrorRepositorySpy.log).toHaveBeenCalledTimes(1)
+      expect(logErrorRepositorySpy.stack).toBe(controllerSpy.httpResponse.body.stack)
     })
   })
 })
