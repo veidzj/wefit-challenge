9dd5191d74859c7dfb66028a1337f42dad4addf9
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-04-01 05:07:10 -0300

    test: ensure LogErrorDecorator does not calls LogErrorRepositoy if controller does not returns a server error

--- tests/main/decorators/log-error-decorator.spec.ts
+++ tests/main/decorators/log-error-decorator.spec.ts
@@ -1,19 +1,23 @@
 import { faker } from '@faker-js/faker'
 
 import { ControllerSpy } from '@/tests/presentation/mocks'
+import { LogErrorRepositorySpy } from '@/tests/application/mocks'
 import { LogErrorDecorator } from '@/main/decorators'
 
 interface Sut {
   sut: LogErrorDecorator
   controllerSpy: ControllerSpy
+  logErrorRepositorySpy: LogErrorRepositorySpy
 }
 
 const makeSut = (): Sut => {
   const controllerSpy = new ControllerSpy()
-  const sut = new LogErrorDecorator(controllerSpy)
+  const logErrorRepositorySpy = new LogErrorRepositorySpy()
+  const sut = new LogErrorDecorator(controllerSpy, logErrorRepositorySpy)
   return {
     sut,
-    controllerSpy
+    controllerSpy,
+    logErrorRepositorySpy
   }
 }
 
@@ -22,20 +26,34 @@ const mockRequest = (): object => ({
 })
 
 describe('LogErrorDecorator', () => {
-  test('Should call Controller with correct values', async() => {
-    const { sut, controllerSpy } = makeSut()
-    const request = mockRequest()
-
-    await sut.handle(request)
-
-    expect(controllerSpy.request).toEqual(request)
+  describe('Controller', () => {
+    test('Should call Controller with correct values', async() => {
+      const { sut, controllerSpy } = makeSut()
+      const request = mockRequest()
+  
+      await sut.handle(request)
+  
+      expect(controllerSpy.request).toEqual(request)
+    })
+  
+    test('Should return the same result of the controller', async() => {
+      const { sut, controllerSpy } = makeSut()
+  
+      const httpResponse = await sut.handle(mockRequest())
+  
+      expect(httpResponse).toEqual(controllerSpy.httpResponse)
+    })
   })
 
-  test('Should return the same result of the controller', async() => {
-    const { sut, controllerSpy } = makeSut()
+  describe('LogErrorRepository', () => {
+    test('Should not call LogErrorRepositoy if controller does not returns a server error', async() => {
+      const { sut, logErrorRepositorySpy } = makeSut()
+      jest.spyOn(logErrorRepositorySpy, 'log')
 
-    const httpResponse = await sut.handle(mockRequest())
+      await sut.handle(mockRequest())
 
-    expect(httpResponse).toEqual(controllerSpy.httpResponse)
+      expect(logErrorRepositorySpy.log).toHaveBeenCalledTimes(0)
+      expect(logErrorRepositorySpy.stack).toBeUndefined()
+    })
   })
 })
